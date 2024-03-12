571de538aca4b02efb19c0fd2469f92e91f48e9c
danielrodrigues <drodrigues@isolutions.com.br>
2024-03-12 12:46:37 +0000

    part 2: article model created in domain

---
+++ lib/core/resources/result.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+abstract class Result<T> {
+  final T ? data;
+  final DioException? error;
+
+  const Result({this.data, this.error});
+}
+
+class DataSuccess<T> extends Result<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends Result<T> {
+  const DataFailed(DioException error) : super(error: error);
+}
